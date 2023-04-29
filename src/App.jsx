8e56300e18b7a2bca0e6de4661bc0298b1cea26e
Samantha S <sami.berlynn@proton.me>
2023-04-28 21:13:02 -0400

    added search/list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const list = [
+  {
+    title: "React",
+    url: "someUrl",
+    author: "author1",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "url2",
+    author: "author2",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
+const App = () => (
+  <div>
+    <h1>Dev Stories</h1>
+    <Search></Search>
+    <hr />
+    <List></List>
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((x) => (
+      <li key={x.objectID}>
+        {x.title}
+        <span>
+          <a href={x.url}>{x.title}</a>
+        </span>
+        <span>{x.author}</span>
+        <span>{x.num_comments}</span>
+        <span>{x.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+const Search = () => {
+  const changeHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input onChange={changeHandler} type="text" id="search" />
+    </div>
+  );
+};
 
-export default App
+export default App;
